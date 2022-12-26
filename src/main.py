3e43c1fc6be252211f87d135c8ddd800214d659a
lycheejam <lycheejamwow@gmail.com>
2022-12-27 01:38:53 +0900

    :+1: スプレッドシートを作成して共有して移動するコードを追加した。

--- src/main.py
+++ src/main.py
@@ -2,11 +2,15 @@ from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
 import os
 import traceback
+import gspread
+from gspread import Client, Spreadsheet
+from pathlib import Path
 
 
 class ManipulationGoogleDrive:
     def __init__(self) -> None:
         self.drive = self.login_with_service_account()
+        self.client = self.authorize_service_account()
 
     def main(self) -> None:
         # テキストファイルを作成して共有設定
@@ -20,6 +24,20 @@ class ManipulationGoogleDrive:
             {"type": "user", "role": "reader", "value": "example@example.com"},
             param={"sendNotificationEmails": False},
         )
+
+        # スプレッドシートを新規作成して共有設定
+        spreadsheet = self.create_spreadsheet("test desu")
+        file_spreadsheet = self.drive.CreateFile({"id": spreadsheet.id})
+        file_spreadsheet.FetchMetadata(fetch_all=True)
+
+        file_spreadsheet.InsertPermission(
+            {"type": "user", "role": "writer", "value": "example@example.com"},
+            param={"sendNotificationEmails": False},
+        )
+
+        # 作成したスプレッドシートの配置場所を移動
+        file_spreadsheet["parents"] = [{"id": "//parent id"}]
+        file_spreadsheet.Upload()
 
     # https://docs.iterative.ai/PyDrive2/oauth/#authentication-with-a-service-account
     def login_with_service_account(self, service_account_file_path: str = ""):
@@ -38,6 +56,27 @@ class ManipulationGoogleDrive:
             gauth = GoogleAuth(settings=settings)
             gauth.ServiceAuth()
             return GoogleDrive(gauth)
+        except Exception:
+            print(traceback.format_exc())
+
+    def authorize_service_account(self, service_account_file_path: str = "") -> Client:
+        if service_account_file_path == "":
+            # NOTICE: SENSITIVE INFO FILE !!!!!
+            service_account_file_path = os.path.join(
+                os.path.dirname(os.path.abspath(__file__)), "../secrets.json"
+            )
+
+        try:
+            return gspread.service_account(filename=Path(service_account_file_path))
+
+        except Exception:
+            print(traceback.format_exc())
+
+    def create_spreadsheet(
+        self, spreadsheet_name: str = "default spreadsheet name"
+    ) -> Spreadsheet:
+        try:
+            return self.client.create(spreadsheet_name)
         except Exception:
             print(traceback.format_exc())
 
